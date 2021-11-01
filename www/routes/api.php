fcbf32f9b7bcb1ce553998b96128b0985c48a5dd
mchev <martin.chevignard@gmail.com>
2021-11-01 10:05:08 +0100

    Electron app

--- www/routes/api.php
+++ www/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\PaletteController;
 use App\Http\Controllers\BathController;
 
@@ -23,10 +24,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('ping', [BathController::class, 'ping']);
-
 Route::get('baths', [BathController::class, 'getNextNumber']);
 
 
+// From manager
 Route::put('account', [AccountController::class, 'update']);
 Route::put('palettes', [PaletteController::class, 'update']);
 
+
+// From App
+Route::put('accounts/{account}/switch', [AccountController::class, 'switch'])->name('accounts.switch');
+Route::put('users/{user}/switch', [UserController::class, 'switch'])->name('users.switch');
