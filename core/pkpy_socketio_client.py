c16c8c58b66687ce92f4f1989a629350e7790205
mchev <martin.chevignard@gmail.com>
2021-11-08 14:15:30 +0100

    handle keyboard input on pi

--- core/pkpy_socketio_client.py
+++ core/pkpy_socketio_client.py
@@ -1,7 +1,7 @@
 import socketio
 import time
 import random
-import keyboard
+from pynput import keyboard
 
 s_io = socketio.Client()
 Tmin = 49.00
@@ -20,18 +20,27 @@ oldDoorState = False
 now = time.time()
 
 # Listen keyboard
-def listen_keyboard():
+def on_press(key):
     global boilerKey
     global boilerState
     global doorKey
     global doorState   
+    global s_io
 
-    if keyboard.read_key() == boilerKey :
+    try:
+        k = key.char  # single-char keys
+    except:
+        k = key.name  # other keys
+    if k == boilerKey :
         print('%s pressed : change boiler State'% boilerKey)
         boilerState = not boilerState
-    if keyboard.read_key() == doorKey :
+    if k == doorKey :
         print('%s pressed : change door State'%doorKey)
         doorState = not doorState
+
+
+listener = keyboard.Listener(on_press=on_press)
+listener.start()  # start to listen on a separate thread
 
 # Try socket connection
 def try_socket_connection(socket_io_client: socketio.Client):
