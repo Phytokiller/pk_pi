46622776055c3a9356c7d3d991552aa963c8399f
mchev <martin.chevignard@gmail.com>
2021-11-10 11:17:28 +0100

    Cors

--- www/routes/api.php
+++ www/routes/api.php
@@ -28,11 +28,15 @@ Route::get('ping', [BathController::class, 'ping']);
 
 
 // From manager
-Route::put('synchronize', [AccountController::class, 'synchronize']);
+Route::middleware(['cors'])->group(function () {
 
-Route::put('account', [AccountController::class, 'update']);
-Route::put('palettes', [PaletteController::class, 'update']);
-Route::put('baths', [BathController::class, 'update']);
+    Route::put('synchronize', [AccountController::class, 'synchronize']);
+
+    Route::put('account', [AccountController::class, 'update']);
+    Route::put('palettes', [PaletteController::class, 'update']);
+    Route::put('baths', [BathController::class, 'update']);
 
 
-Route::get('baths', [BathController::class, 'index']);
+    Route::get('baths', [BathController::class, 'index']);
+
+});
