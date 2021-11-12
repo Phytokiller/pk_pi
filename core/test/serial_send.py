91f7ea30972e8d33db3890527264613a045ef5e1
acetayls <pierrick.saillant@yahoo.fr>
2021-11-12 14:55:37 +0100

    test arduino

---
+++ core/test/serial_send.py
@@ -0,0 +1,16 @@
+import serial
+import time
+
+ser = serial.Serial(port='/dev/ttyAMA3', baudrate=38400))
+ser.open()
+
+def send(message) :
+	byte_to_send = bytes(message, 'utf-8')
+	ser.write(byte_to_send)
+
+while True :
+	send('test1')
+	time.sleep(1)
+	send('test2')
+	time.sleep(2)
+	
