eab7b6dd1936f85f439a170802780f3b00dde2ec
acetayls <pierrick.saillant@yahoo.fr>
2021-11-15 10:46:48 +0100

    add startup script

--- startup/install_service.sh
+++ startup/install_service.sh
@@ -4,12 +4,12 @@ sudo cp pk_chromium.service /etc/systemd/system/
 
 
 sudo systemctl --system daemon-reload
-sudo systemctl start pk_server.service
+
 sudo systemctl enable pk_server.service
+sudo systemctl restart pk_server.service
 
-sudo systemctl start pk_client_ardbox.service
 sudo systemctl enable pk_client_ardbox.service
+sudo systemctl restart pk_client_ardbox.service
 
-sudo systemctl start pk_chromium.service
-sudo systemctl enable chromium.service
-
+sudo systemctl enable pk_chromium.service
+sudo systemctl restart pk_chromium.service
