9d96203793c9d8a7fc22fda940eec9d95579adb5
mchev <martin.chevignard@gmail.com>
2021-11-16 17:16:08 +0100

    Change label

--- www/resources/js/Pages/Setting.vue
+++ www/resources/js/Pages/Setting.vue
@@ -33,7 +33,7 @@
           <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
           </svg>
-          Redémarrer
+          Redémarrer la station
         </button>
         <button @click="sendUpdateCmd" type="button" class="mt-4 p-3 bg-indigo-800 rounded text-white text-2xl">
           <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
