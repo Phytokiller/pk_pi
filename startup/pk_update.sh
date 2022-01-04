04906d42cf8a54836c1dabcbd34c0ba5558a5c94
mchev <martin.chevignard@gmail.com>
2022-01-04 17:29:17 +0100

    Permission

--- startup/pk_update.sh
+++ startup/pk_update.sh
@@ -1,7 +1,9 @@
-sudo chown www-data:www-data /var/www
+sudo addgroup www-data
+sudo usermod -a -G www-data pi
+sudo chown -R pi:www-data /var/www
+sudo chmod -R 755 /var/www/html/pk_pi/www/database/database.sqlite
 sudo chmod -R 777 /var/www/html/pk_pi/www/bootstrap/cache
 sudo chmod -R 777 /var/www/html/pk_pi/www/storage
-sudo usermod -a -G www-data pi
 
 cd /var/www/html/pk_pi
 
