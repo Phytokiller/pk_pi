842de0d2210c9484d2cbbf4d691e8b5599990cda
mchev <martin.chevignard@gmail.com>
2022-01-04 19:18:28 +0100

    Git not following file permissions

--- startup/pk_update.sh
+++ startup/pk_update.sh
@@ -1,12 +1,13 @@
 sudo addgroup www-data
 sudo usermod -a -G www-data pi
 sudo chown -R :www-data /var/www
-sudo chmod -R 775 /var/www/html/pk_pi/www
+#sudo chmod -R 775 /var/www/html/pk_pi/www
 sudo chmod -R 777 /var/www/html/pk_pi/www/bootstrap/cache
 sudo chmod -R 777 /var/www/html/pk_pi/www/storage
 
 cd /var/www/html/pk_pi
 
+git config --local core.fileMode false
 git reset --hard && git clean -df
 
 git pull origin master
