b7c5cd44aa1dbf795bc45a87d3cca1289762de61
mchev <martin.chevignard@gmail.com>
2022-01-05 13:22:27 +0100

    Palette selector show the latest palette

--- www/app/Http/Controllers/WelcomeController.php
+++ www/app/Http/Controllers/WelcomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Palette;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\Paginator;
 use Inertia\Inertia;
 
 class WelcomeController extends Controller
@@ -14,19 +15,34 @@ class WelcomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+
+        if($request->page == '') {
+
+            $lastPage = $this->pk->currentAccount()
+                        ->palettes()
+                        ->with('bath')
+                        ->latest()
+                        ->paginate(1)
+                        ->lastPage();
+
+            Paginator::currentPageResolver(function() use ($lastPage) {
+                return $lastPage;
+            });
+            
+        }
 
         return Inertia::render('Welcome', [
             'palettes' => $this->pk->currentAccount()
                         ->palettes()
                         ->with('bath')
-                        ->orderBy('created_at', 'desc')
+                        ->latest()
                         ->paginate(1),
             'default' => $this->pk->currentAccount()
                         ->palettes()
                         ->whereDoesntHave('bath')
-                        ->orderBy('created_at')
+                        ->orderBy('created_at', 'ASC')
                         ->limit(2)
                         ->get(),
         ]);
