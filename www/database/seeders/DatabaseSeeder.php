c3e656011266c23dc862a06c9c8b453f254d9d22
mchev <martin.chevignard@gmail.com>
2023-01-17 15:31:00 +0100

    Account seed

--- www/database/seeders/DatabaseSeeder.php
+++ www/database/seeders/DatabaseSeeder.php
@@ -15,6 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+        \App\Models\Account::create([
+            'name' => 'PK'
+        ]);
+
         \App\Models\Setting::create([
 
         ]);
