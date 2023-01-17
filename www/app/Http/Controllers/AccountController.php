d69a570b4899c61b46d8ea92878491a03a9adb6b
mchev <martin.chevignard@gmail.com>
2023-01-17 16:46:30 +0100

    Wip

--- www/app/Http/Controllers/AccountController.php
+++ www/app/Http/Controllers/AccountController.php
@@ -28,8 +28,6 @@ class AccountController extends Controller
             $counter = $request->account['bath_counter'];
         }
 
-        return response()->json($counter);
-
         // Update account data from manager
         $account = Account::updateOrCreate([
             'id' => $request->account['id'],
@@ -39,7 +37,7 @@ class AccountController extends Controller
             'bath_temperature' => $request->account['bath_temperature'],
             'bath_number_prefix' => $request->account['bath_number_prefix'],
             'default_palettes_selected' => $request->account['default_palettes_selected'],
-            'bath_counter' => ($request->account['bath_counter'] >= $this->pk->currentAccount()->bath_counter) ? $request->account['bath_counter'] : $this->pk->currentAccount()->bath_counter,
+            'bath_counter' => $counter,
             'updated_at' => Carbon::now(),
         ]);
 
